67c11f90629ea7940ab324a22b68838303b31322
Khabubu Phathutshedzo <khabubuphathu@gmail.com>
2019-11-30 01:23:10 +0200

    feature : Added InitialMigration

---
+++ HealthForAll/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HealthForAll.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        private readonly IConfigurationRoot configurationRoot;
+
+        public ApplicationDbContextFactory()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            configurationRoot = configurationBuilder.Build();
+        }
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseSqlServer(configurationRoot.GetConnectionString("DefaultConnection"),
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(ApplicationDbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
