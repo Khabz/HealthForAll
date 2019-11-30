b5d7b8f014ed6254f342cace66530033fe5f4df5
Khabubu Phathutshedzo <khabubuphathu@gmail.com>
2019-11-30 15:44:04 +0200

    feature : Added Modal

--- HealthForAll/Models/HomeViewModels/HomeViewModel.cs
+++ HealthForAll/Models/HomeViewModels/HomeViewModel.cs
@@ -12,6 +12,21 @@ namespace HealthForAll.Models.HomeViewModels
         public UserRestModel CurrentUser { get; set; }
 
         public class UserRestModel {
+
+            [JsonProperty("id")]
+            public string Id { get; set; }
+
+            [JsonProperty("first_name")]
+            public string FirstName { get; set; }
+
+            [JsonProperty("last_name")]
+            public string LastName { get; set; }
+
+            [JsonProperty("gender")]
+            public Gender Gender { get; set; }
+
+            [JsonProperty("membership_type")]
+            public MembershipType MembershipType { get; set; }
         }
     }
 }
