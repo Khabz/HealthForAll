cdfb5b7f47da174be705f9f415c328741ec8ca04
Khabubu Phathutshedzo <khabubuphathu@gmail.com>
2019-11-30 12:18:41 +0200

    feature : Added Image

--- HealthForAll/Models/HomeViewModels/HomeViewModel.cs
+++ HealthForAll/Models/HomeViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace HealthForAll.Models.HomeViewModels
 {
     public class HomeViewModel
     {
+        [JsonProperty("current_user")]
+        public UserRestModel CurrentUser { get; set; }
+
+        public class UserRestModel {
+        }
     }
 }
